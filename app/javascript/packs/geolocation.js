adc5b3fa909322643a60726632171f020143c1e6
naonao-program <gf19a012@oecu.jp>
2021-12-18 22:39:05 +0900

    半径を選択し、現在地から店名、アクセス、サムネイル画像を取得することができた。

--- app/javascript/packs/geolocation.js
+++ app/javascript/packs/geolocation.js
@@ -2,6 +2,7 @@
   window.addEventListener('load',function() {
     const click = document.getElementById('btn')
     click.addEventListener('click', function(){
+      document.getElementById('results').innerHTML = '検索結果'
       const ranges = document.form1.ranges;
       const num = ranges.selectedIndex;
       const API_KEY = gon.hotpepper_key;
@@ -10,27 +11,17 @@
       function successCallback(position) {
         var longitude = position.coords.longitude;
         var latitude = position.coords.latitude;
-        const url = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=' + API_KEY + '&lng=' + longitude + '&lat=' + latitude + '&range' + num + '&order=4'
+        const url = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=' + API_KEY + '&lng=' + longitude + '&lat=' + latitude + '&range=' + num + '&order=4' + '&format=json' + '&count=100'
 
-        
-        // 取得
-        // var request = new XMLHttpRequest();
-        // request.open('GET', url, true);
-        // request.responseType = 'json';
-        // request.onload = function () {
-        //   var data = this.response;
-        //   console.log(data);
-        // }
-        // // リクエストをURLに送信
-        // request.send();
-        
         fetch(url,{
         }).then(function(response) {
-          return response.text();
-        }).then(function(text) {
-          
-          console.log(text);
-          document.getElementById('page').innerHTML = text
+          return response.json();
+        }).then(function(json) {
+          var a = '';
+          for(var i = 0; i < JSON.stringify(json.results.shop.length); i++){
+            a +=  '<li>' + '<a>' + '<img src =' + JSON.stringify(json.results.shop[i].logo_image) + '>' +'店名'+ JSON.stringify(json.results.shop[i].name) + 'アクセス' + JSON.stringify(json.results.shop[i].access) + '</a>' +'</li>'
+          }
+          document.getElementById('page').innerHTML = a;
         });
       }
       function errorCallback(error) {
@@ -47,7 +38,7 @@
             err_msg = "タイムアウトしました";
             break;
         }
-        console.log(err_msg);
+        document.getElementById('page').innerHTML = err_msg;
       }
     })
   });
