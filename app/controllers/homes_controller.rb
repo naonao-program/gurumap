9373f66300840e3cb843fa2246b90509d9af987e
naonao-program <gf19a012@oecu.jp>
2021-12-19 22:29:18 +0900

    jsを使って選択されたサムネイル画像 店名 住所 営業時間を表示できるように実装した。

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,9 +1,14 @@
 class HomesController < ApplicationController
+  before_action :hotpepper_key, only:[:search, :show]
   def search
-    gon.hotpepper_key = ENV['HOTPEPPER_API']
   end
 
   def show
     
+  end
+  
+  private
+  def hotpepper_key
+    gon.hotpepper_key = ENV['HOTPEPPER_API']
   end
 end
--- app/javascript/packs/geolocation.js
+++ app/javascript/packs/geolocation.js
@@ -1,16 +1,19 @@
+var page = location.href;
+const API_KEY = gon.hotpepper_key;
+
+
 window.addEventListener('load',function() {
   const click = document.getElementById('btn')
   click.addEventListener('click', function(){
     document.getElementById('results').innerHTML = '検索結果'
     const ranges = document.form1.ranges;
     const num = ranges.selectedIndex;
-    const API_KEY = gon.hotpepper_key;
       // 現在地の取得
     navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
     function successCallback(position) {
       var longitude = position.coords.longitude;
       var latitude = position.coords.latitude;
-      const url = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=' + API_KEY + '&lng=' + longitude + '&lat=' + latitude + '&range=' + num + '&order=4' + '&format=json' + '&count=100'
+      const url = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=' + API_KEY + '&lng=' + longitude + '&lat=' + latitude + '&range=' + num  + '&format=json' + '&count=100'
       // JSONを取得
       fetch(url,{
       }).then(function(response) {
@@ -18,11 +21,12 @@ window.addEventListener('load',function() {
       }).then(function(json) {
         var a = '';
         for(var i = 0; i < JSON.stringify(json.results.shop.length); i++){
-          a +=  '<li>' + '<a href="homes/' + i + '">' + '<img src =' + JSON.stringify(json.results.shop[i].logo_image) + '>' +'店名'+ JSON.stringify(json.results.shop[i].name) + 'アクセス' + JSON.stringify(json.results.shop[i].access) + '</a>' +'</li>'
+          a +=  '<li>' + '<a href="homes/' + i + '" id= "text">' + '<img src =' + JSON.stringify(json.results.shop[i].logo_image) + '>' +'店名'+ JSON.stringify(json.results.shop[i].name) + 'アクセス' + JSON.stringify(json.results.shop[i].access) + '</a>' +'</li>'
         }
         document.getElementById('page').innerHTML = a;
 
         // お店が10件以上見つかったらページネーションをする
+
       });
     }
     function errorCallback(error) {
